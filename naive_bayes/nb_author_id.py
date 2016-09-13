b29a9ec192c1b1f273a6903b29c26b149b8318c5
Srikalyan Swayampakula <srikalyan.swayampakula@locationlabs.com>
2016-09-13 09:19:24 -0700

    Added gaussian naive base logic to train on sample set and predict the results.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,7 +13,7 @@
 import sys
 from time import time
 sys.path.append("../tools/")
-from email_preprocess import preprocess
+from tools.email_preprocess import preprocess
 
 
 ### features_train and features_test are the features for the training
@@ -26,6 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+clf = GaussianNB()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+predicted_lables = clf.predict(features_test)
+print "Prediction time: ", round(time() - t1, 3), "s"
+
+accuracy = accuracy_score(labels_test, predicted_lables)
+
+print accuracy
+
 
 
 #########################################################
