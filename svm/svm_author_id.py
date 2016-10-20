8f9234d478134010952dcc043368089e4c3c7ab9
Srikalyan Swayampakula <srikalyansswayam@gmail.com>
2016-10-19 22:30:06 -0700

    finished chapter 2 on svm and quiz

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,18 +22,21 @@ from sklearn.svm import SVC
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000)
+
 
 t0 = time()
 clf.fit(features_train, labels_train)
-print "Training time:", round(time()-t0, 3), "s"
-
+print "Training time:", round(time() - t0, 3), "s"
 
 t1 = time()
 pred = clf.predict(features_test)
-print "Prediction time:", round(time()-t1, 3), "s"
+print "Prediction time:", round(time() - t1, 3), "s"
 
 accuracy = accuracy_score(labels_test, pred)
 
 print "Accuracy is {}".format(accuracy)
+
+
+print len([ i for i in pred if i == 1])
 
